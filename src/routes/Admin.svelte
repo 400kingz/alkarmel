4315a02969acfe87930f7e1686a0861f066c9a01
The Professor <119928642+400kingz@users.noreply.github.com>
2025-05-12 15:18:09 -0700

    Fix admin profile and header navigation

--- src/routes/Admin.svelte
+++ src/routes/Admin.svelte
@@ -67,7 +67,7 @@
     labels: ['January', 'February', 'March', 'April', 'May', 'June'],
     datasets: [{
       label: 'Sales',
-      data: [65, 59, 80, 81, 56, 55],
+      data: [65, 59, 80, 81, 90, 100],
       fill: false,
       borderColor: 'rgb(75, 192, 192)',
       tension: 0.1
